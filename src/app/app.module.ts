4fd34c814c36f0fbffaf97be5d546464ec284ea3
Yonatan <yonatan.ramirez557@gmail.com>
2018-07-25 11:30:34 -0700

    first push

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { LandpageComponent } from './landpage/landpage.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    LandpageComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/landpage/landpage.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-landpage',
+  templateUrl: './landpage.component.html',
+  styleUrls: ['./landpage.component.scss']
+})
+export class LandpageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
